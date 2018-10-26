64ffe21bfc11a64d3990085ccf5212b99c73a16b
Artsiom Kaptur <Artsiom_Kaptur@epam.com>
2018-10-26 10:57:07 +0300

    Add TestListener

---
+++ src/main/java/com/epam/mentoringprogramm/listener/TestListener.java
@@ -0,0 +1,22 @@
+package com.epam.mentoringprogramm.listener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class TestListener implements IInvokedMethodListener {
+    private static Logger logger = LogManager.getLogger();
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        logger.info("method started: " + method.getTestMethod().getMethodName());
+
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        logger.info("method finished [" + testResult.getStatus() + "]: " + method.getTestMethod().getMethodName() + "\n");
+    }
+}
