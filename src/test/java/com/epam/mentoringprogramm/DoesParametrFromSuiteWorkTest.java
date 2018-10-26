96861e54d84e3ef96f4d162b69ae59c88d59377b
Artsiom Kaptur <Artsiom_Kaptur@epam.com>
2018-10-26 12:47:26 +0300

    Add test with parameter from suite xml

---
+++ src/test/java/com/epam/mentoringprogramm/DoesParametrFromSuiteWorkTest.java
@@ -0,0 +1,16 @@
+package com.epam.mentoringprogramm;
+
+import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class DoesParametrFromSuiteWorkTest {
+
+    @Parameters({"message"})
+    @Test
+    public void doesParametrFromSuiteWork(@Optional(value = "Something went wrong") String message) {
+        Assert.assertEquals(message, "Hurrey! Parametr works!");
+    }
+
+}
